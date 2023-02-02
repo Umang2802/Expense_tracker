06ef8f92991fc217de8631bf5f0bc32c4d92774d
Umang-Metri <umang_metri@persistent.com>
2023-02-02 23:26:30 +0530

    added deplyment code in backend

--- backend/app.js
+++ backend/app.js
@@ -26,10 +26,10 @@ const path = require("path");
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "client/build")));
+  app.use(express.static(path.join(__dirname1, "frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
